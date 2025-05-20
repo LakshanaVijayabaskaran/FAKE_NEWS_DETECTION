d4d91f49e1dacbace07f5a9ca3b6d961ab479a16
LAKSHANA.V <161549336+LakshanaVijayabaskaran@users.noreply.github.com>
2025-05-20 20:27:05 +0530

    Update App.js

--- fake-news-ui/src/App.js
+++ fake-news-ui/src/App.js
@@ -12,7 +12,7 @@ function App() {
 const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await fetch(${process.env.REACT_APP_API_BASE_URL}/predict,, {
+    const res = await fetch('https://fake-news-detection-5-myxi.onrender.com/predict',  {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text }),
